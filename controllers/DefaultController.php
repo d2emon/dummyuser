477d7147892f2cf5b8d923739e083e3029f6ee37
Dmitry Kutsenko <d2emonium@gmail.com>
2016-07-21 11:45:57 +0300

    Fix redirect

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -118,7 +118,7 @@ class DefaultController extends Controller
         $model = new User();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['view', 'username' => $model->username]);
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -137,7 +137,7 @@ class DefaultController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['view', 'username' => $model->username]);
         } else {
             return $this->render('update', [
                 'model' => $model,
